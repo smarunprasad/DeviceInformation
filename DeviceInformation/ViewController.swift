cb5f9cc8f288cf3820c5e2ee8369ce5b3ea5639e
Arunprasat Selvaraj <smarun16@gmail.com>
2019-06-25 18:34:00 +0100

    Added the Ravlin API and send it to the class

--- DeviceInformation/ViewController.swift
+++ DeviceInformation/ViewController.swift
@@ -12,6 +12,17 @@ import EncryptedAPIManager
 
 class ViewController: UIViewController {
 
+    
+    let timestamp = "timestamp"
+    let customer = "customer"
+    let customerId = "customerId"
+    let email = "email"
+    let name = "name"
+    let deviceBody = "device"
+    let deviceId = "deviceId"
+    let custom = "custom"
+    let deviceInfo = "deviceInfo"
+    
     let device = EncryptedDeviceInfo()
     var encryptAPIManager: EncryptedAPIManager!
     var deviceData = Data()
@@ -20,45 +31,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        
         setUpModel()
     }
 
     
     func setUpModel() {
         
-       encryptTheDeviceData()
+       callAPIWithEncryptDeviceData()
     }
 
-    func getDeviceInfo() {
-        
-        device.getDeviceInfo { (device) in
-            
-            var param = [String: Any]()
-            param["name"] = device.name
-            param["model"] = device.model
-            param["systemName"] = device.systemName
-            param["systemVersion"] = device.systemVersion
-
-            if let data = self.deviceInfoDictGenerator(parameters: param) {
-                self.deviceData = data
-            }
-        }
-    }
-    
-    
-    func encryptTheDeviceData() {
-        
-        getDeviceInfo()
+    private func callAPIWithEncryptDeviceData() {
         
         do {
-
             encryptAPIManager = try EncryptedAPIManager.init(key: "FiugQTgPNwCWUY,VhfmM4cKXTLVFvHFe")
-            
-            encryptAPIManager.getEncryptedData(data: self.deviceData, completionBlock: { (encryptedData) in
+            encryptAPIManager.getEncryptedData(data: generateHTTPBody(), completionBlock: { (encryptedData) in
                 
-                var request = URLRequest.init(url: URL.init(string: "https://api.github.com/gists/ac57abaea4faba3e3cb6bf45e733c670")!)
-                request.httpMethod = "get"
+                var request = URLRequest.init(url: URL.init(string: "https://api-staging.ravelin.com/v2/customer")!)
+                request.httpMethod = "post"
+                request.allHTTPHeaderFields = generateHTTPHeader()
+                request.httpBody = encryptedData
 
                 encryptAPIManager.encryptedAPIRequest(request: request, completionHandler: { (handler) in
 
@@ -92,6 +83,69 @@ class ViewController: UIViewController {
         catch {
             print(error)
         }
+    }
+
+    private func generateHTTPBody() -> Data{
+        
+        /*{
+         "timestamp": 1512828988826,
+         "customer": {
+         "customerId": "smarun16@gmail.com",
+         "email": "smarun16@gmail.com",
+         "name": "Arun"
+         },
+         "device": {
+         "deviceId": "65fc5ac0-2ba3-4a3b-aa5e-f5a77b845260",
+         "custom": {
+         "deviceInfo": {}
+         }
+         }*/
+        
+        //Variables
+        var customBodyObject = [String: Any]()
+        var customerObject = [String: Any]()
+        var deviceObject = [String: Any]()
+        var customObject = [String: Any]()
+
+        //Custom Object
+        customerObject[customerId] = "smarun16@gmail.com"
+        customerObject[email] = "smarun16@gmail.com"
+        customerObject[name] = "Arun"
+        
+        //Custom Object
+        customObject[deviceInfo] = getDeviceInfo()
+
+        //Device object
+        deviceObject[deviceId] = "65fc5ac0-2ba3-4a3b-aa5e-f5a77b845260"
+        deviceObject[custom] = customObject
+        
+        //customBody Object
+        customBodyObject[timestamp] = 1512828988826
+        customBodyObject[customer] = customerObject
+        customBodyObject[deviceBody] = deviceObject
+        
+        
+        guard let data = self.deviceInfoDictGenerator(parameters: customBodyObject) else {
+            return Data()
+        }
+        return data
+    }
+    private func generateHTTPHeader() -> [String: String] {
+    
+        return ["Authorization":"token secret_key_live_1Od41KPkFXqn9gR4KB8s5l3hvOaGAYqs",
+                "Content-Type":"application/json"]
+    }
+    private func getDeviceInfo() -> [String: Any] {
+        
+        var param = [String: Any]()
+        device.getDeviceInfo { (device) in
+            
+            param["name"] = device.name
+            param["model"] = device.model
+            param["systemName"] = device.systemName
+            param["systemVersion"] = device.systemVersion
+        }
+        return param
     }
     
     private func deviceInfoDictGenerator(parameters: [String: Any]) -> Data? {
