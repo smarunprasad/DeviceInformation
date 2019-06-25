b770b37c7cc32ce87064e2b5130cf32e5e404a1c
Arunprasat Selvaraj <smarun16@gmail.com>
2019-06-26 00:52:48 +0100

    Final Commit

--- DeviceInformation/ViewController.swift
+++ DeviceInformation/ViewController.swift
@@ -11,7 +11,6 @@ import DeviceKit
 import CryptKit
 
 class ViewController: UIViewController {
-
     
     let timestamp = "timestamp"
     let customer = "customer"
@@ -23,10 +22,20 @@ class ViewController: UIViewController {
     let custom = "custom"
     let deviceInfo = "deviceInfo"
     
+    //Model
     let device = DeviceInfo()
     var cryptManager: CryptManager!
     var deviceData = Data()
-
+    var logString = ""
+    
+    //Outlets
+    @IBOutlet weak var deviceInfoImageView: UIImageView!
+    
+    @IBOutlet weak var encryptionImageView: UIImageView!
+    
+    @IBOutlet weak var apiImageView: UIImageView!
+    @IBOutlet weak var logTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,40 +52,56 @@ class ViewController: UIViewController {
         }
     }
     
-    //MARK: Encryption Methode call
-    func encryptData(data: Data) -> Data {
+    //MARK: UIButton Action
+    @IBAction func startButtonTapped(_ sender: Any) {
         
-        var cryptData = Data()
-        cryptManager.getEncryptedData(data: data, completionBlock: { (data) in
-           
-            if let encrptedData = data {
-                cryptData = encrptedData
-            }
-        }) { (error) in
-            
-        }
-        return cryptData
+        //1
+        //Get device information
+        ///////////////
+        let dictionary = getDeviceInfo()
+        self.loadLogInTextView(value: dictionary)
+        
+        //2
+        //Converting to data type
+        //////////////
+        guard let data = generateDataFrom(parameters: dictionary) else { return  }
+        self.logString += "\nDevice info data\n"
+        self.loadLogInTextView(value: data)
+        self.deviceInfoImageView.isHighlighted = true
+        
+        //3
+        //Encryption
+        //////////////
+        deviceData = encryptData(data: data)
+        self.logString += "\nEncrypted device info data\n"
+        self.loadLogInTextView(value: self.deviceData)
+        self.encryptionImageView.isHighlighted = true
+        
+        //4
+        //Sending the device data and static values to the API
+        //////////////
+        callCustomerAPI()
     }
     
-    //MARK: Decryption Methode call
-    func decryptData(data: Data) -> Data {
+    @IBAction func resetButtonTapped(_ sender: Any) {
         
-        var cryptData = Data()
-        cryptManager.getDecryptedData(encrptedData: data, completionBlock: { (data) in
-            if let decrptedData = data {
-                cryptData = decrptedData
-            }
-        }) { (error) in
+        UIView.transition(with: self.view, duration: 0.1, options: .transitionCrossDissolve, animations: {
             
-        }
-        return cryptData
+            self.logString = ""
+            self.logTextView.text = ""
+            self.deviceData = Data()
+            
+            self.deviceInfoImageView.isHighlighted = false
+            self.encryptionImageView.isHighlighted = false
+            self.apiImageView.isHighlighted = false
+        }, completion: nil)
     }
 }
 
-//MARK:- Encryption Device Info
+//MARK:- Device Info Method
 extension ViewController {
     
-    func getEncryptedDeviceInfo() -> Data {
+    func getDeviceInfo() -> [String: Any] {
         
         var param = [String: Any]()
         device.getDeviceInfo { (device) in
@@ -86,8 +111,7 @@ extension ViewController {
             param["systemName"] = device.systemName
             param["systemVersion"] = device.systemVersion
         }
-        guard let data = generateDataFrom(parameters: param) else { return Data() }
-        return data
+        return param
     }
 }
 
@@ -95,7 +119,7 @@ extension ViewController {
 extension ViewController {
     
     //MARK: HTTP Body
-    func generateHTTPBody(deviceData: Data) -> Data{
+    func generateHTTPBody() -> Data{
         
         //Variables
         var customBodyObject = [String: Any]()
@@ -109,7 +133,7 @@ extension ViewController {
         customerObject[name] = "Arun"
         
         //Custom Object
-        customObject[deviceInfo] = deviceData
+        customObject[deviceInfo] = deviceData.base64EncodedString()
         
         //Device object
         deviceObject[deviceId] = "65fc5ac0-2ba3-4a3b-aa5e-f5a77b845260"
@@ -138,12 +162,21 @@ extension ViewController {
             return
         }
         var urlRequest = URLRequest.init(url: URL.init(string: "https://api-staging.ravelin.com/v2/customer")!)
-        urlRequest.httpBody = generateHTTPBody(deviceData: encryptData(data: getEncryptedDeviceInfo()))
+        urlRequest.httpBody = generateHTTPBody()
         urlRequest.httpMethod = "post"
         urlRequest.allHTTPHeaderFields = generateHTTPHeader()
         
         APIManager.dataRequest(request: urlRequest, completionHandler: { (data) in
             
+            self.logString += "\n Response data\n"
+            self.loadLogInTextView(value: data)
+            
+            self.logString += "\n Response String\n"
+            self.loadLogInTextView(value: String.init(data: data, encoding: .utf8) as Any)
+            
+            DispatchQueue.main.async {
+                self.apiImageView.isHighlighted = true
+            }
             
         }) { (error) in
             
@@ -153,6 +186,64 @@ extension ViewController {
 
 //MARK:- Helper
 extension ViewController {
+    
+    //MARK: Encryption Methode call
+    func encryptData(data: Data) -> Data {
+        
+        var cryptData = Data()
+        cryptManager.getEncryptedData(data: data, completionBlock: { (data) in
+            
+            if let encrptedData = data {
+                cryptData = encrptedData
+            }
+        }) { (error) in
+            
+        }
+        return cryptData
+    }
+    
+    //MARK: Decryption Methode call
+    func decryptData(data: Data) -> Data {
+        
+        var cryptData = Data()
+        cryptManager.getDecryptedData(encrptedData: data, completionBlock: { (data) in
+            if let decrptedData = data {
+                cryptData = decrptedData
+            }
+        }) { (error) in
+            
+        }
+        return cryptData
+    }
+    
+    //MARK: Support functions
+    
+    func loadLogInTextView(value: Any) {
+        
+        if value is [String: Any] {
+            
+            logString = "DeviceInfo\n"
+            
+            //To print the device info in log textview
+            for (x, y) in value as! [String: Any] {
+                logString += "(\(x): \(y))\n"
+            }
+        }
+        else if value is Data {
+            
+            let data = value as! Data
+            logString += "\(data.count)"
+        }
+        else {
+            
+            logString += value as! String
+        }
+        DispatchQueue.main.async {
+            
+            self.logTextView.text = self.logString
+        }
+    }
+    
     
     func generateDataFrom(parameters: [String: Any]) -> Data? {
         
